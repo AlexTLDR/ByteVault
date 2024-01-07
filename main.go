0077d3d1dbe76d08bc8ce472faf97c255593b901
AlexTLDR <alex.badragan@protonmail.com>
2024-01-07 13:25:44 +0100

    forcing POST for snippetCreate

--- main.go
+++ main.go
@@ -6,14 +6,35 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello World"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a new snippet"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting server on :3000")
-	err := http.ListenAndServe(":3000", mux)
+	log.Print("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
