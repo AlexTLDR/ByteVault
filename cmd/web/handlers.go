524dee955d0f013767b5a671e51cd5a82c4dd333
AlexTLDR <alex.badragan@protonmail.com>
2024-01-12 11:15:03 +0100

    create snippet form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -52,7 +52,9 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("TODO: new snippet form"))
+	data := app.newTemplateData(r)
+
+	app.render(w, r, http.StatusOK, "create.html", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
