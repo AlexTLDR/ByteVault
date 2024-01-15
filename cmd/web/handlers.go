8dd3988fb4c343edd21c18ca29561eae731e46fe
AlexTLDR <alex.badragan@protonmail.com>
2024-01-15 13:50:17 +0100

    using the validator helpers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,18 +5,17 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 
 	"github.com/AlexTLDR/ByteVault/internal/models"
+	"github.com/AlexTLDR/ByteVault/internal/validator"
 	"github.com/julienschmidt/httprouter"
 )
 
 type snippetCreateForm struct {
-	Title       string
-	Content     string
-	Expires     int
-	FieldErrors map[string]string
+	Title   string
+	Content string
+	Expires int
+	validator.Validator
 }
 
 // using the templateData struct holding the snippet data in templates.go for rendering multiple pieces of data
@@ -84,28 +83,19 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	}
 	// creating the insert data
 	form := snippetCreateForm{
-		Title:       r.PostForm.Get("title"),
-		Content:     r.PostForm.Get("content"),
-		Expires:     expires,
-		FieldErrors: map[string]string{},
+		Title:   r.PostForm.Get("title"),
+		Content: r.PostForm.Get("content"),
+		Expires: expires,
 	}
 
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "Title cannot be blank"
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-		form.FieldErrors["title"] = "Title limit of 100 characters exceeded"
-	}
+	form.CheckField(validator.NotBlank(form.Title), "title", "Title cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "Title limit of 100 characters exceeded")
+	form.CheckField(validator.NotBlank(form.Content), "content", "Content field cannot be blank")
+	form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
 
-	if strings.TrimSpace(form.Content) == "" {
-		form.FieldErrors["content"] = "Content field cannot be blank"
-	}
-
-	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
-		form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
-	}
 	// if there are validation errors, re-display the create.html template
 	// HTTP status code 422 (Unprocessable Entity) is used
-	if len(form.FieldErrors) > 0 {
+	if !form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, r, http.StatusUnprocessableEntity, "create.html", data)
